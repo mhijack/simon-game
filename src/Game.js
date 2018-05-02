d2ec6f42af23db3ba3da3dd26e169002a4d41ad7
mhijack <jianyuan.chen12@gmail.com>
2018-05-01 21:04:54 -0700

    add essential  functionalities

--- src/Game.js
+++ src/Game.js
@@ -6,6 +6,8 @@ import './Game.css';
 //  1. ! present button presses to follow
 //  2. ! if clicked `color !== this.state.sequence[stepCount]`, report error (stepCount incremented by 1 at each click, and reset if everything's okay)
 //  3. ! when color button is clicked, 1 play sound 2 log color
+//  4. ! if pressed wrong button, restart series (with same sequence)
+//  5. ! start game with one additional color in sequence
 
 const GREEN = '#00a74a';
 const RED = '#9f0f17';
@@ -35,13 +37,14 @@ class Game extends Component {
 		super(props);
 		this.state = {
 			colors: COLORARR,
-			sequence: [RED, BLUE, YELLOW],
+			sequence: [],
 			stepCount: 0,
-			buttonClickable: false
+			buttonClickable: false,
+			intervalKey: null
 		};
 	}
 
-	// light up each button in sequence & play sound
+	// present user with random button presses
 	lightUp = () => {
 		const colors = this.state.colors;
 		let sequence = this.state.sequence;
@@ -49,10 +52,8 @@ class Game extends Component {
 
 		const interval = setInterval(() => {
 			let colorIndex = colors.indexOf(sequence[count]);
-			console.log(colorIndex);
 			// play sound
-			SOUNDS[colorIndex].load();
-			SOUNDS[colorIndex].play();
+			playSoundAtIndex(colorIndex);
 			// retrieve button to add 'lightup' class
 			let button = document.getElementById(colorIndex);
 			button.classList.add('lightup');
@@ -63,14 +64,23 @@ class Game extends Component {
 			count += 1;
 			if (count >= sequence.length) {
 				clearInterval(interval);
-				console.log('interval cleared');
+				console.log('lightup interval cleared');
 			}
-		}, 1200);
+    }, 1200);
+    // set intervalKey for clearing setInterval when click wrong button
+		this.setState({ intervalKey: interval });
 	};
 
 	// when click 'start' button, display sequence & set button to clickable once finished
-	startGame = () => {
-    this.setState({ stepCount: 0})
+	startGame = addNewColor => {
+		if (addNewColor) {
+			// at beginning of each round, push one random color to sequence
+			const sequence = this.state.sequence;
+			sequence.push(generateNextColor(COLORARR));
+      this.setState({ sequence });
+      console.log('added')
+		}
+		// play sequence
 		this.lightUp();
 		// wait until lightup finished then make button clickable
 		setTimeout(() => {
@@ -78,40 +88,43 @@ class Game extends Component {
 		}, 1200 * this.state.sequence.length + 500);
 	};
 
+	restartSequence = () => {
+		// 1 clear interval using this.state.intervalKey
+		// 2 wait 500ms to restart game // TODO - flash indicator to indicate wrong input
+		clearInterval(this.state.intervalKey);
+    this.setState({
+      buttonClickable: false,
+      intervalKey: null,
+      stepCount: 0
+    })
+    this.startGame(false);
+	};
+
 	handleColorMouseDown = (color, index) => {
 		// color is color of the button
 		// index to play sound
-
+		const stepCount = this.state.stepCount;
+		const sequence = this.state.sequence;
 		// .load() fixes problem of audio playing only once
-		SOUNDS[index].load();
-		SOUNDS[index].play();
-		console.log(color);
-		if (color === this.state.sequence[this.state.stepCount]) {
-			// if correct, continue checking next input
-			console.log('correct');
-		} else {
-			// otherwise, reset game // TODO - reset game
-			console.log('wrong');
+		playSoundAtIndex(index);
+		if (color !== sequence[stepCount]) {
+			// if clicked on wrong color, reset game // TODO - reset game
+      this.restartSequence();
+      return;
 		}
 
-		// check if last color in sequence. if so, restart game // TODO - start game with one additional color in sequence
-		const newStepCount = this.state.stepCount + 1;
-		if (newStepCount === this.state.sequence.length) {
+		// check if last color in sequence. if so, restart game
+		const newStepCount = stepCount + 1;
+		if (newStepCount === sequence.length) {
 			// if at last sequence, 1 reset stepCount, 2 add a new color to sequence, and 3 call start() to restart game
-			const sequence = this.state.sequence;
-			sequence.push(generateNextColor(COLORARR));
-			console.log(sequence);
-			this.setState(
-				prevState => {
-          return {
-            stepCount: 0,
-            sequence: sequence,
-            buttonClickable: false
-          }
-        }
-			);
-      this.startGame();
-      return;
+			this.setState(prevState => {
+				return {
+					stepCount: 0,
+					buttonClickable: false
+				};
+			});
+			this.startGame(true);
+			return;
 		}
 		this.setState({ stepCount: newStepCount });
 	};
@@ -132,7 +145,10 @@ class Game extends Component {
 		return (
 			<div>
 				{buttons}
-				<button onClick={this.startGame}>Start</button>
+				<button className="startBtn" onClick={this.startGame}>
+					Start
+				</button>
+        <p>{this.state.sequence.length}</p>
 			</div>
 		);
 	}
@@ -144,4 +160,10 @@ export default Game;
 // generates a random color inside COLORARR
 function generateNextColor(colors) {
 	return colors[Math.floor(Math.random(colors.length) * 4)];
+}
+
+// play audio
+function playSoundAtIndex(index) {
+  SOUNDS[index].load();
+  SOUNDS[index].play();
 }
