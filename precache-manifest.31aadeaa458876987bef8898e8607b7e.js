652e94248bbd0e9c9540244c9155c99d496262d8
Jianyuan Chen <jianyuan.chen12@gmail.com>
2018-11-03 18:14:08 -0700

    Updates

---
+++ precache-manifest.31aadeaa458876987bef8898e8607b7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c64644f9a906fad8f7a5473120702ed",
+    "url": "/simon-game/static/media/twitter.2c64644f.png"
+  },
+  {
+    "revision": "ce1747dc3c6073e37847",
+    "url": "/simon-game/static/js/runtime~main.ce1747dc.js"
+  },
+  {
+    "revision": "60cde81b945c2a546b57",
+    "url": "/simon-game/static/js/main.60cde81b.chunk.js"
+  },
+  {
+    "revision": "901d1c6b7b98b31ce275",
+    "url": "/simon-game/static/js/1.901d1c6b.chunk.js"
+  },
+  {
+    "revision": "60cde81b945c2a546b57",
+    "url": "/simon-game/static/css/main.51f35adb.chunk.css"
+  },
+  {
+    "revision": "7ece7c28170ff46c771f6800cfbb7788",
+    "url": "/simon-game/index.html"
+  }
+];
